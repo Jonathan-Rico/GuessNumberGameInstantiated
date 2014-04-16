ead2f1ed2fb964d9573daf4ce426dfa64358b975
a208 <a208@ISA2080010.sac.rsccd.inst>
2014-04-15 19:48:23 -0700

    ???

---
+++ RandomNumber/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+			RandomNumber Random = new RandomNumber();
+			Random.randomNumber();
+	}
+}
---
+++ RandomNumber/src/RandomNumber.java
@@ -0,0 +1,6 @@
+public class RandomNumber {
+	public void randomNumber () {
+	int x =	(int) (1 + Math.random() * 10);
+	System.out.printf("%d", x);
+	}
+}
