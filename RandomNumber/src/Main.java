950004532a56e8e8ca7047fbfa64784ec6122172
a208 <a208@ISA2080010.sac.rsccd.inst>
2014-04-15 20:08:20 -0700

    ???

--- RandomNumber/src/Main.java
+++ RandomNumber/src/Main.java
@@ -1,7 +1,21 @@
 
+import java.util.Scanner;
 public class Main {
-	public static void main(String[] args) {
-			RandomNumber Random = new RandomNumber();
-			Random.randomNumber();
-	}
-}
+	public static void main(String [] args){
+		System.out.printf("Welcome to the guess a number game, please type in a number between 1 and 10\n");		
+		RandomNumber randomNumber = new RandomNumber();
+		randomNumber.Number();
+		
+		Scanner input = new Scanner(System.in);		
+		int user = input.nextInt();		
+		if (user == randomNumber.Number()){			
+			System.out.printf("Congratulations, the number %d is the correct choice. You have won the game.\n", user);		
+			}		
+		else if (user < randomNumber.Number()){			
+			System.out.printf("Sorry but the number %d is too low, the correct number is %d, please try again by restarting program", user , randomNumber.Number());		
+			}		
+		else if (user > randomNumber.Number()){			
+			System.out.printf("Sorry but the number %d is too high, the correct number is %d, please try again by restarting program", user , randomNumber.Number());			
+			}	
+		}
+	} 
--- RandomNumber/src/RandomNumber.java
+++ RandomNumber/src/RandomNumber.java
@@ -1,6 +1,6 @@
 public class RandomNumber {
-	public void randomNumber () {
+	public int Number () {
 	int x =	(int) (1 + Math.random() * 10);
-	System.out.printf("%d", x);
+	return x;
 	}
 }
